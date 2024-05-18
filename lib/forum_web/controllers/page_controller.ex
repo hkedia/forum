be35c4dd5e64c9197586979d1e5c2d2597ca8d4b
Harsh Kedia <hkedia@hey.com>
2024-05-18 23:05:44 +0530

    creating posts

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,12 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Alice", email: "alice@example.com"},
+      %{id: 2, name: "Bob", email: "bob@example.com"}
+    ]
+    json(conn, %{users: users})
+  end
 end
